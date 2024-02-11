95dcbf5eee955d1ddb0590926ca83c7c6871fec6
Tomasz <n1kula@wp.pl>
2024-02-11 15:40:06 +0100

    show data in cw

--- CustomerOrderViewer/Program.cs
+++ CustomerOrderViewer/Program.cs
@@ -10,11 +10,25 @@ namespace CustomerOrderViewer
     {
         static void Main(string[] args)
         {
-            CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=XPS\SQLEXPRESS;Initial Catalog=CustomerOrderViewer;Integrated Security=True;");
-            IList<CustomerOrderDetailModel> customerOrderDetialModels = customerOrderDetailCommand.GetList();
-            foreach (var model in customerOrderDetialModels)
+            try
             {
-                Console.WriteLine(model.FirstName);
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=XPS\SQLEXPRESS;Initial Catalog=CustomerOrderViewer;Integrated Security=True;");
+                IList<CustomerOrderDetailModel> customerOrderDetialModels = customerOrderDetailCommand.GetList();
+                foreach (CustomerOrderDetailModel model in customerOrderDetialModels)
+                {
+                    Console.WriteLine("{0}: Fullname: {1} {2} (id: {3}) - purchased {4} for {5} (id: {6})",
+                        model.CustomerOrderId,
+                        model.FirstName,
+                        model.LastName,
+                        model.CustomerId,
+                        model.Description,
+                        model.Price,
+                        model.ItemId
+                        );
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
