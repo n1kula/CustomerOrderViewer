31df18a9675557486417cc23e50681311052e64e
Tomasz <n1kula@wp.pl>
2024-02-11 13:37:26 +0100

    view added

---
+++ CustomerOrderViewer/view.sql
@@ -0,0 +1,29 @@
+﻿
+USE CustomerOrderViewer
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+PRINT 'CustomerOrderDetail'
+GO
+
+
+CREATE VIEW [dbo].[CustomerOrderDetail] AS
+	SELECT
+		co.CustomerOrderID,
+		c.CustomerId,
+		c.FirstName,
+		c.LastName,
+		i.Description,
+		i.Price
+	FROM
+		dbo.CustomerOrder co
+	INNER JOIN 
+		dbo.Customer c ON co.CustomerId = c.CustomerId
+	INNER JOIN
+		dbo.Item i ON i.ItemId = co.ItemId
+GO
